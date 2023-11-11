122aa317a3a4a2f7cddf51cedca2cc6d3e8ca7a0
fdicera <67300276+fdicera@users.noreply.github.com>
2023-11-11 18:04:45 +0000

    Added labels and button to Movies page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,13 @@
 class MoviesController < ApplicationController
+  def create
+    # Retrieve user's inputs from params
+    # Create a record in the movie table
+    # Populate eac hcolumn with user input
+    # Save
+
+    # Redirect the user back to the /movies URL
+  end
+  
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
